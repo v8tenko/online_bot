ae5e4bce6ac01aab5b142dc2b8ef51f31501134b
Gleb Voitenko <v8tenko@gmail.com>
2023-03-29 12:02:41 +0000

    chore: initiaize linters

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+	executablePath: '/usr/bin/chromium-browser',
+	args: ['--no-sandbox']
+});
 const page = await browser.newPage();
+
+await page.goto('https://web.telegram.org/k');
+
+await page.screenshot();
+
+await browser.close();
